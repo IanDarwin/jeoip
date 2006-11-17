a514feeeae1ba1e0e98654b0d13b793cdd331c8d
IanDarwin <>
2006-11-17 17:21:32 +0000

    Continent is an enum and should be treated as such all through the code.

--- src/jeoip/Continent.java
+++ src/jeoip/Continent.java
@@ -1,9 +1,23 @@
 package jeoip;
 
 public enum Continent {
-	NORTH_AMERICA,
-	SOUTH_AMERICA,
-	EUROPE,
-	ASIA,
-	AUSTRALIA
+	AFRICA("Africa"),
+	ANTARCTICA("Antarctica"),
+	ASIA_PACIFIC("Asia/Pacific"),
+	AUSTRALIA("Australia"),
+	EUROPE("Europe"),
+	NORTH_AMERICA("North America"),
+	OFF_CONTINENT("Off Continent"),
+	SOUTH_AMERICA("South America"),
+	UNKNOWN("Unknown");
+
+	String name;
+
+	Continent(String d) { name = d; }
+
+	public static Continent byName(String desc) {
+		if ("--".equals(desc))
+				return Continent.UNKNOWN;
+		return Continent.valueOf(desc.replaceAll("[ /]", "_").toUpperCase());
+	}
 }
--- src/jeoip/Country.java
+++ src/jeoip/Country.java
@@ -1,22 +1,26 @@
 package jeoip;
 
 /**
- * Simple holder for the Country name, codes, continents, etc.
+ * Simple bean for the Country name, codes, continents, etc.
  */
 public class Country {
-
+	/** The 2-letter ISO-3166 country code */
 	String code2;
-
+	/** The 3-letter ISO-3166 country code */
 	String code3;
-
+	/** Country Name */
 	String name;
+	/** The continent */
+	Continent continent;
 
-	String continent;
-
-	public Country(String name, String code2, String code3, String continent) {
+	public Country(String name, String code2, String code3, Continent continent) {
 		this.name = name;
 		this.code2 = code2;
 		this.code3 = code3;
 		this.continent = continent;
+	}
+
+	public Country(String name, String code2, String code3, String continent) {
+		this(name, code2, code3, Continent.byName(continent));
 	}
 }
